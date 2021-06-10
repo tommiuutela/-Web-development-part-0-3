32bcbbf9e2572158b64aa0f627cc85ea0cb55513
Tommi Uutela <tommiuutela45@gmail.com>
2021-06-10 16:24:43 +0300

    1.4

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,58 +1,65 @@
 import React from 'react'
 
-const Header = (props) => {
-  console.log('THIS IS MY PROPS INSIDE HEADER' , props)
-
-  return(
-    <h1>{props.topic}</h1>
-  )
-}
-
-
-const Content = (props) => {
-  console.log(props)
-  return (
-    <>
-    <Part part={props.part1} exercises={props.exercises1}/>
-    <Part part={props.part2} exercises={props.exercises2}/>
-    <Part part={props.part3} exercises={props.exercises3}/>
-    </>
-  )
-}
-const Part = (props) =>{
-  return (
-      <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-      </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <>
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </>
-  )
-}
-
 const App = () => {
-  console.log('this is clog in app');
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const Header = () => {
+    console.log()
+  
+    return(
+      <h1>{course}</h1>
+    )
+  }
+  
+  
+  const Content = () => {
+    console.log()
+    return (
+      <>
+      <Part part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part part={parts[2].name} exercises={parts[2].exercises}/>
+      <Part part={parts[3].name} exercises={parts[3].exercises}/>
+      </>
+    )
+  }
+  const Part = (props) =>{
+    return (
+        <>
+        <p>
+          {props.part} {props.exercises}
+        </p>
+        </>
+    )
+  }
+  
+  const Total = (props) => {
+    return (
+      <>
+      <p>Number of exercises {parts[1].exercises + parts[2].exercises + parts[3].exercises}</p>
+      </>
+    )
+  }
 
   return (
     <div>
-      <Header topic={course} />
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} 
-       exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header/>
+      <Content/>
+      <Total/>
+      
     </div>
   )
 }
