a7a44980e7d2c3da72bf88f438ab5dcf78b8f6ee
Tommi Uutela <tommiuutela45@gmail.com>
2021-06-17 15:28:17 +0300

    done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react' 
+import Note from './components/Note'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    ''
+  )
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Phonebook</h1>
+      
+      <form onSubmit={addNote}>
+        <p>Name:</p>
+        <input value={newNote}
+        onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>   
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+      {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
