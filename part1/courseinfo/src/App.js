9ffad473c394cd64bf362856b652dbcf0d9385ce
Tommi Uutela <tommiuutela45@gmail.com>
2021-06-17 15:29:49 +0300

    done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   const Total = () => {
     return (
       <>
-      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
       </>
     )
   }
